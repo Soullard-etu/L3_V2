178ca6919b716b23e466af86ba39a12bb4e6abf8
Soullard-etu <soullard.etu@gmail.com>
2023-09-07 13:49:48 +0200

    first commit

---
+++ atelier/presentation personnage/presentation.c
@@ -0,0 +1,18 @@
+/*
+John McCarthy était un informaticien américain renommé, connu principalement pour son rôle pionnier 
+dans le domaine de l'intelligence artificielle (IA) et de l'informatique. Il est né le 4 septembre 1927 à Boston, 
+Massachusetts, et est décédé le 24 octobre 2011.
+
+John McCarthy est célèbre pour avoir inventé le terme "intelligence artificielle" (IA) en 1955, 
+lorsqu'il a organisé la Conférence de Dartmouth, qui est souvent considérée comme le point de départ 
+de la recherche en IA. Il a également développé le langage de programmation LISP (List Processing), 
+qui est devenu l'un des langages les plus importants pour la recherche en IA.
+
+Tout au long de sa carrière, John McCarthy a contribué à de nombreux aspects de l'informatique 
+et de l'intelligence artificielle, notamment la logique mathématique, la programmation symbolique, 
+la recherche sur les réseaux de neurones artificiels et bien d'autres domaines. 
+Il a reçu de nombreuses récompenses et distinctions pour ses contributions à l'informatique et à l'IA.
+
+En résumé, John McCarthy était un informaticien et chercheur américain de premier plan qui a joué un rôle essentiel 
+dans le développement de l'intelligence artificielle et de l'informatique moderne.
+*/
